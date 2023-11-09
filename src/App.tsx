af202bada052426d895eba3b0f02670b9bd8c411
DANI BACHINI <dani_costa90@yahoo.com.br>
2023-11-09 21:27:45 +0100

    webpack and tailwind configured

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './static/input.css';
+
+export default function App() {
+    return (
+        <div className='text-lg text-rose-600'>
+          Hello World !!!!! working ??????
+        </div>
+    );
+}
